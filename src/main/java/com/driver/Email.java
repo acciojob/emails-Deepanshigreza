1ccaae209c78f2393ce406dc24f7f15c7dd01d7f
Deepanshigreza <deepanshigreza@gmail.com>
2022-12-24 20:12:44 +0530

    done

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,32 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(this.password.equals(oldPassword)) {
+            if(meetCondition(newPassword)) this.password = newPassword;
+        }
+
+    }
+    boolean meetCondition(String s) {
+        boolean condition1=false,condition2=false,condition3=false,condition4=false,condition5=false;
+        if(s.length()>=8) condition1 = true;
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            condition2 |= isUpperCase(ch);
+            condition3 |= isLoweCase(ch);
+            condition4 |= isDigit(ch);
+            condition5 |= (!isUpperCase(ch) && !isLoweCase(ch) && !isDigit(ch));
+        }
+        return condition1 && condition2 && condition3 && condition4 && condition5;
+    }
+    boolean isUpperCase(char ch){
+        return ch>='A' && ch<='Z';
+    }
+
+    boolean isLoweCase(char ch) {
+        return ch>='a' && ch<='z';
+    }
+
+    boolean isDigit(char ch) {
+        return ch>='0' && ch<='9';
     }
 }
